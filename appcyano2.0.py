9adf18cab60af4de668b24ddae1911650a7697d2
jdissipative <juand.castrob@outlook.com>
2025-02-25 10:18:29 -0500

    Update appcyano2.0.py

--- appcyano2.0.py
+++ appcyano2.0.py
@@ -84,7 +84,7 @@ GHI=GHI/1000
 st.title("Dynamical System Model")
 left_col, spacer, right_col = st.columns([1, 0.2, 2])
 with left_col:
-    params = {param: st.slider(param, 0.00, 10.0, 0.5, 0.001, format="%.5f") for param in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'Kc', 'Kt', 'z']}
+    params = {param: st.slider(param, 0.00, 100.0, 0.5, 0.001, format="%.5f") for param in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'Kc', 'Kt', 'z']}
     C0 = st.number_input("Initial Cyanobacteria (C0)", value=0.005, format="%.6f")
     P0 = st.number_input("Initial Phosphorus (P0)", value=0.005, format="%.6f")
     UMBRAL = st.number_input("Umbral", value=0.5, format="%.6f")
